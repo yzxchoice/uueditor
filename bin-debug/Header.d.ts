d3610b19bd4ae5193b5e06d422a87e3d28b720bc
yzxchoice <yzxchoice@sina.com>
2018-08-14 10:29:18 +0800

    ''

--- bin-debug/Header.d.ts
+++ bin-debug/Header.d.ts
@@ -9,6 +9,7 @@ declare class Header extends eui.Group implements IUUContainer {
     private onPostComplete(event);
     private onPostIOError(event);
     private onPostProgress(event);
+    onAddPage(event: egret.TouchEvent): void;
     preview(event: egret.TouchEvent): void;
     openComponentPanel(event: egret.TouchEvent): void;
     openSoundPanel(event: egret.TouchEvent): void;
